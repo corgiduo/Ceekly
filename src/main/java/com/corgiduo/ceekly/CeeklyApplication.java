9f5dcb98e2950ebb5c93c2e2d71031de3cde23c9
ms-dev <qiheng.liu@outlook.com>
2020-07-07 00:13:50 +0800

    initial spring boot project.

---
+++ src/main/java/com/corgiduo/ceekly/CeeklyApplication.java
@@ -0,0 +1,13 @@
+package com.corgiduo.ceekly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeeklyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeeklyApplication.class, args);
+    }
+
+}
